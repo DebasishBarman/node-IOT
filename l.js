21b0c2118400295e9cfd63484277285d05587078
Debasish Barman <debdeva298@gmail.com>
2021-11-29 21:01:32 +0530

    Update l.js

--- l.js
+++ l.js
@@ -7,18 +7,27 @@ var myport=new SerialPort(portName,{
     baudRate:115200,
     parsers:SerialPort.parsers
 })
+const Readline=SerialPort.parsers.Readline
+var parser=new Readline()
 
+myport.pipe(parser)
 myport.on('open',onOpen)
-// myport.write('Hellpo')
 myport.on('data',onData)
 
 function onOpen(){
 console.log('Open Connection');
+    myport.write('4',(err,res)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(res);
+    })
 }
 function onData(data){
     console.log('Data is '+data);
 
 }
+
 
 
 // myport.
